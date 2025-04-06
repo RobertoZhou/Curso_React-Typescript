4e56b2804695551ef666383bc2087bea12daf1ab
Zhou <141890618+RobertoZhou@users.noreply.github.com>
2025-04-06 00:31:22 -0300

    useRef

--- aulas/src/app/pages/dashboard/Dashboard.tsx
+++ aulas/src/app/pages/dashboard/Dashboard.tsx
@@ -1,9 +1,16 @@
+import { useRef } from 'react';
 import { Link } from 'react-router-dom';
 
 export const Dashboard = () => {
+    const counterRef = useRef({ counter: 0 });
+
     return (
         <div>
             <p>Dashboard</p>
+
+            <p>Contador: {counterRef.current.counter}</p>
+            <button onClick={() => counterRef.current.counter++}>Somar</button>
+            <button onClick={() => console.log(counterRef.current.counter)}>Mostrar no console</button>
             <Link to='/login'>Login</Link>
         </div>
     );
--- aulas/src/app/pages/login/Login.tsx
+++ aulas/src/app/pages/login/Login.tsx
@@ -1,27 +1,52 @@
 import { Console } from "console";
-import { useState } from "react"
+import { useCallback, useEffect, useMemo, useRef, useState } from "react"
 
 export const Login = () => {
+    const inputSenhaRef = useRef<HTMLInputElement>(null);
+
     const [email, setEmail] = useState('');
     const [senha, setSenha] = useState('');
 
-    const handleEntrar = () => {
-        console.log(email)
+    // useEffect(() => {
+    //     if(window.confirm('Você é homem?')) {
+    //         console.log("homem");
+    //     } else {
+    //         console.log("Mulher");
+    //     } 
+    // }, []);
+
+    // useEffect(() => {
+    //     console.log(email);
+    // }, [email]);
+
+    // useEffect(() => {
+    //     console.log(senha);
+    // }, [senha]);
+
+    const emailLength = useMemo(() => {
+        return (
+            email.length * 10000
+        )
+    }, [email.length]);
+
+    const handleEntrar = useCallback(() => {
+        console.log(email);
         console.log(senha);
-    }
+    }, [email, senha]);
 
     return (
         <div>
             <form>
+                <p>Quantidade de caracteres no email: {emailLength}</p>
 
                 <label>
                     <span>Email</span>
-                    <input value={email} onChange={e => setEmail(e.target.value)} type="email" />
+                    <input onKeyDown={e => e.key === 'Enter' ? inputSenhaRef.current?.focus() : undefined} value={email} onChange={e => setEmail(e.target.value)} type="email" />
                 </label>
 
                 <label>
                     <span>Senha</span>
-                    <input value={senha} onChange={e => setSenha(e.target.value)} type="password" />
+                    <input ref={inputSenhaRef}  value={senha} onChange={e => setSenha(e.target.value)} type="password" />
                 </label>
 
                 <button type="button" onClick={handleEntrar}>
